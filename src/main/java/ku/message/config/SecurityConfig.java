17971f9c3ddd3b0614349861c6ae86cc6f850a8b
Tetach Rattanavikran <47658385+theethaj@users.noreply.github.com>
2021-05-08 10:05:58 +0700

    Initialized Commit

---
+++ src/main/java/ku/message/config/SecurityConfig.java
@@ -0,0 +1,56 @@
+package ku.message.config;
+
+import ku.message.service.UserDetailsServiceImp;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserDetailsServiceImp userDetailsService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/home", "/signup", "/css/**", "/js/**")
+                .permitAll()
+                .antMatchers("/product/add").access("hasRole('ROLE_ADMIN')")
+                .antMatchers("/product", "/message", "/post")
+                .access("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
+                .anyRequest().authenticated();
+
+        http.formLogin()
+                .defaultSuccessUrl("/message", true)
+                .and().logout();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth)
+            throws Exception {
+        auth.userDetailsService(userDetailsService)
+                .passwordEncoder(encoder());
+    }
+
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder(13);
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web
+                .ignoring()
+                .antMatchers("/h2-console/**");
+    }
+}
---
+++ src/main/java/ku/message/controller/ProductController.java
@@ -0,0 +1,38 @@
+package ku.message.controller;
+
+import ku.message.model.Product;
+import ku.message.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/product")
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping
+    public String getProductPage(Model model) {
+        model.addAttribute("products", productService.getAllProducts());
+        return "product";
+    }
+
+    @GetMapping("/add")
+    public String getAddProductPage(Model model) {
+        return "product-add";
+    }
+
+    @PostMapping
+    public String createProduct(@ModelAttribute Product product,
+                                Model model) {
+        productService.createProduct(product);
+        return "redirect:/product";
+    }
+
+}
