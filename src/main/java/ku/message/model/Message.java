edb598e5bb8efd7cf271d1ea8389170b46b5a380
theethaj <bank30827@gmail.com>
2021-05-08 10:09:06 +0700

    Add random ID

--- src/main/java/ku/message/model/Message.java
+++ src/main/java/ku/message/model/Message.java
@@ -1,41 +1,21 @@
 package ku.message.model;
-
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.util.UUID;
 
+@Data
+@NoArgsConstructor
 @Entity
 public class Message {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private UUID id;
 
     private String user;
     private String text;
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getUser() {
-        return user;
-    }
-
-    public void setUser(String user) {
-        this.user = user;
-    }
-
-    public String getText() {
-        return text;
-    }
-
-    public void setText(String text) {
-        this.text = text;
-    }
 }
--- src/main/java/ku/message/model/Product.java
+++ src/main/java/ku/message/model/Product.java
@@ -1,33 +1,14 @@
 package ku.message.model;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
 public class Product {
 
     private long id;
-
     private String name;
     private double price;
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
 }
+
